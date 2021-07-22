2abddf87810322963f58501b80cabe27bdb4e94a
Jason <exanix@gmail.com>
2021-07-22 12:28:44 -0700

    upd(event-bus): add `update-menu` and `args` support

--- src/state/event-bus.ts
+++ src/state/event-bus.ts
@@ -1,22 +1,24 @@
 
 
-type ValidEvents = 'update-footer';
-const _events: [ValidEvents] = ['update-footer'];
-const _subscribers: [event: string, callback: () => void][] = [];
+type ValidEvents = 'update-footer'|'update-menu';
+type CallBack    = (...args: any[]) => void;
+
+const _events = ['update-footer', 'update-menu'];
+const _subscribers: [event: string, cb: CallBack][] = [];
 
 
 export function useEventBus() {
   return {
-    on: (ev: ValidEvents, cb: () => void) => {
+    on: (ev: ValidEvents, cb: CallBack) => {
       const addedSub = _events.includes(ev) && !!_subscribers.push([ev, cb]);
       if (!addedSub) throw Error(`Missing Event::"${ev}"`);
     },
-    off: (cb: () => void) => {
+    off: (cb: CallBack) => {
       _subscribers.forEach((sub, i) => sub[1] == cb && _subscribers.splice(i, 1));
     },
-    emit: (ev: ValidEvents) => {
+    emit: (ev: ValidEvents, ...args: any[]) => {
       if (_events.includes(ev)) {
-        _subscribers.forEach(sub => sub[0] == ev && sub[1]());
+        _subscribers.forEach(sub => sub[0] == ev && sub[1](...args));
         return;
       }
       throw Error(`Missing Event::"${ev}"`);
