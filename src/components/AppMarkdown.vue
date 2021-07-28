9e02138f2204c32716290412eae52664d5158155
Jason <exanix@gmail.com>
2021-07-28 13:46:30 -0700

    AppMarkdown: add `simple` flag for basic markdown support

--- src/components/AppMarkdown.vue
+++ src/components/AppMarkdown.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="md">
+  <div
+    :class="[
+      { 'md': !simple },
+      { 'md-default md-em md-bold': simple }
+    ]"
+  >
     <slot />
   </div>
 </template>
 
-<script lang='ts' setup></script>
+<script lang='ts' setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    simple: { type: Boolean, default: false }
+  });
+</script>
+
