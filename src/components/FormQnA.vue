4cc956f182f4df8eff685c70ebc808b57e3124c0
Jason <exanix@gmail.com>
2021-08-05 16:08:25 -0700

    upd(FormQnA): convert to `script setup`
    ref #65

--- src/components/FormQnA.vue
+++ src/components/FormQnA.vue
@@ -2,11 +2,11 @@
   <div class="qnaf ux__page-container">
     <div class="qnaf__form">
       <ux-input
-        v-model="name"
+        v-model="formData.name"
         class="qnaf__input"
         :minchars="3"
         :maxchars="20"
-        :validate="validateInput"
+        :validate="validate"
         :regex="nameRegex"
         :tally="true"
         :errmsg="'<em>Special characters</em> are not allowed'"
@@ -16,17 +16,17 @@
       <br>
 
       <ux-input
-        v-model="email"
+        v-model="formData.email"
         class="qnaf__input"
         type="email"
-        :validate="validateInput"
+        :validate="validate"
       >
         E-Mail
       </ux-input>
       <br>
 
       <div
-        v-for="(q, i) of questionRefs"
+        v-for="(q, i) of questions"
         :key="i"
         class="qnaf__q-block"
       >
@@ -48,7 +48,7 @@
           :minchars="minchars"
           :maxchars="maxchars"
           :placeholder="'Answer here...'"
-          :validate="validateInput"
+          :validate="validate"
         />
       </div>
       <div class="qnaf__controls">
@@ -62,20 +62,20 @@
         </ux-button>
         <ux-button
           :theme="'attention'"
-          :disabled="!areInputsValidated"
+          :disabled="!isValidated"
           :loading="isSubmitting"
           @click="submit"
         >
           SUBMIT
         </ux-button>
-        <ux-text v-if="remainingInputs > 0" class="qnaf__validation-text">
-          <strong>{{ remainingInputs }}</strong> field(s) require(s) attention
+        <ux-text v-if="remaining > 0" class="qnaf__validation-text">
+          <strong>{{ remaining }}</strong> field(s) require(s) attention
         </ux-text>
         <form-error
           v-else
           class="qnaf__error-text"
-          :update="errorUpdate"
-          :text="errorText"
+          :update="formState.errorUpdate"
+          :text="formState.errorText"
         />
       </div>
     </div>
@@ -83,15 +83,15 @@
 </template>
 
 
-<script lang='ts'>
-import { defineComponent, PropType, reactive, toRefs } from "@vue/runtime-core";
+<script lang='ts' setup>
+import { PropType, reactive, defineProps, defineEmits } from "@vue/runtime-core";
 import useInputValidation from "@/composeables/inputValidation";
 import { APIErrorResp, useAPI }         from "@/services/api_internal";
-import uxButtonVue        from "./UxButton.vue";
-import FormErrorVue     from "./FormSubmitError.vue";
-import uxInputVue         from "./UxInput.vue";
-import uxTextVue          from "./UxText.vue";
 import { DataCacheArrayKeys, useDateCache } from "@/state/cache-state";
+import UxButton        from "./UxButton.vue";
+import FormError     from "./FormSubmitError.vue";
+import UxInput         from "./UxInput.vue";
+import UxText          from "./UxText.vue";
 
 
 export type FormQuestion = { text: string; subtext?: string; answer?: string; }
@@ -102,76 +102,65 @@ interface ReactiveQuestion {
   subtext? : string;
 }
 
-
-export default defineComponent({
-  components: {
-    'ux-text'       : uxTextVue,
-    'ux-input'      : uxInputVue,
-    'ux-button'     : uxButtonVue,
-    'form-error'    : FormErrorVue,
-  },
-  props: {
-    id        : { type: String  as PropType<DataCacheArrayKeys>,         required: true       },
-    type      : { type: Number  as PropType<number>,         required: true       },
-    questions : { type: Array   as PropType<FormQuestion[]>, default: () => []    },
-    nameLabel : { type: String  as PropType<string>,         default: 'Your Name' },
-    showBack  : { type: Boolean as PropType<boolean>,        default: false       },
-    minchars  : { type: Number  as PropType<number>,         default: 100         },
-    maxchars  : { type: Number  as PropType<number>,         default: 500         },
-  },
-  emits: ['back', 'submitted'],
-  setup(props, ctx) {
-    const api             = useAPI();
-    const cache           = useDateCache<ReactiveQuestion>();
-    const nameRegex       = /^[a-z\s.]+$/i;
-    const oldQuestions    = cache.getArrayData(props.id).value;
-    const questions       = oldQuestions.length ? oldQuestions : getReactiveQuestions();
-    const inputValidation = useInputValidation(2 + props.questions.length);
-    const formData        = reactive({ name: '', email: '', });
-    const formState       = reactive({ errorUpdate: 0, errorText: '' });
-
-    if (!props.questions.length) throw Error('qnaform::Missing Questions');
-    if (!oldQuestions.length) cache.setArrayData(props.id, questions);
-
-    function submit() {
-      const qData = {
-        ...formData,
-        type: props.type,
-        questions: questions.map(q => ({ text: q.text, answer: q.answer }))
-      };
-      api
-        .post('/form/qna', qData)
-        .then(() => {
-          // Clear answers in cache
-          cache.setArrayData(props.id, getReactiveQuestions());
-          ctx.emit('submitted');
-        })
-        .catch(setFormError)
-      ;
-    }
-
-    function getReactiveQuestions() {
-      return props.questions.map(q => reactive({ ...q, answer: q.answer || ''}));
-    }
-
-    function setFormError(err: APIErrorResp) {
-      formState.errorUpdate = Date.now();
-      formState.errorText = err.message;
-    }
-
-    return {
-      ...toRefs(formData),
-      ...toRefs(formState),
-      validateInput: inputValidation.validate,
-      remainingInputs: inputValidation.remaining,
-      areInputsValidated: inputValidation.isValidated,
-      questionRefs: questions,
-      isSubmitting: api.isPending,
-      submit,
-      nameRegex,
-    };
-  }
+const props           = defineProps({
+  id        : { type: String  as PropType<DataCacheArrayKeys>, required: true       },
+  type      : { type: Number  as PropType<number>,             required: true       },
+  questions : { type: Array   as PropType<FormQuestion[]>,     default: () => []    },
+  nameLabel : { type: String  as PropType<string>,             default: 'Your Name' },
+  showBack  : { type: Boolean as PropType<boolean>,            default: false       },
+  minchars  : { type: Number  as PropType<number>,             default: 100         },
+  maxchars  : { type: Number  as PropType<number>,             default: 500         },
 });
+const emit            = defineEmits(['back', 'submitted']);
+const api             = useAPI();
+const cache           = useDateCache<ReactiveQuestion>();
+const nameRegex       = /^[a-z\s.]+$/i;
+const oldQuestions    = cache.getArrayData(props.id).value;
+const questions       = oldQuestions.length ? oldQuestions : getReactiveQuestions();
+const formData        = reactive({ name: '', email: '', });
+const formState       = reactive({ errorUpdate: 0, errorText: '' });
+
+if (!props.questions.length) throw Error('<form-qna>::Missing Questions');
+if (!oldQuestions.length) cache.setArrayData(props.id, questions);
+
+
+const {
+  remaining,
+  validate,
+  isValidated
+} = useInputValidation(2 + props.questions.length);
+
+const isSubmitting = api.isPending;
+
+
+function submit() {
+  const qData = {
+    ...formData,
+    type: props.type,
+    questions: questions.map(q => ({ text: q.text, answer: q.answer }))
+  };
+  api
+    .post('/form/qna', qData)
+    .then(() => {
+      // Clear answers in cache
+      cache.setArrayData(props.id, getReactiveQuestions());
+      emit('submitted');
+    })
+    .catch(setFormError)
+  ;
+}
+
+
+function getReactiveQuestions() {
+  return props.questions.map(q => reactive({ ...q, answer: q.answer || ''}));
+}
+
+
+function setFormError(err: APIErrorResp) {
+  formState.errorUpdate = Date.now();
+  formState.errorText = err.message;
+}
+
 
 </script>
 
