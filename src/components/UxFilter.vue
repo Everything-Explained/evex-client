dc42fa127f69e0b010f2ab238b20e1bb5d914c68
Jason <exanix@gmail.com>
2021-08-05 15:34:33 -0700

    upd(UxFilter): convert to `script setup`
    * Refactors pageFilter into composeable
    
    ref #65

--- src/components/UxFilter.vue
+++ src/components/UxFilter.vue
@@ -38,147 +38,43 @@
 
 
 
-<script lang="ts">
-import { defineComponent, PropType, ref } from "vue";
-import { DataCacheFilterObj, useDateCache } from "@/state/cache-state";
+<script lang="ts" setup>
+import { PropType, defineProps, defineEmits } from "vue";
 import { useEventBus } from "@/state/event-bus";
+import { FilterData, usePageFilter } from "@/composeables/pageFilter";
 // Components
-import uxCheckboxVue  from "./UxCheckbox.vue";
-import uxIconVue      from "./UxIcon.vue";
-import uxToggleVue    from "./UxToggle.vue";
+import UxCheckbox  from "./UxCheckbox.vue";
+import UxIcon      from "./UxIcon.vue";
+import UxToggle    from "./UxToggle.vue";
 
-interface FilterData {
-  // Allow ignorable props
-  [key: string]: any;
-  author: string;
-  date: string;
-}
-
-export default defineComponent({
-  components: {
-    'ux-checkbox' : uxCheckboxVue,
-    'ux-toggle'   : uxToggleVue,
-    'ux-icon'     : uxIconVue,
-  },
-  props: {
-    ageOnly:      { type: Boolean as PropType<boolean>,    default: false,             },
-    reverseOrder: { type: Boolean as PropType<boolean>,    default: false,             },
-    items:        { type: Array as PropType<FilterData[]>, default: [] as FilterData[] },
-    persist:      { type: Boolean as PropType<boolean>,    default: true               },
-  },
-  emits: ['filter'],
-  setup({items, persist, reverseOrder}, {emit}) {
-    const isChecked        = ref([]);
-    const eventBus         = useEventBus();
-
-    const {
-      toggleFilter,
-      filterAuthor,
-      reversePages,
-      authors,
-      isFilterOpen,
-      areItemsReversed,
-      filteredPages,
-      authorIndexMap,
-    } = usePageFilter(items, persist, reverseOrder);
-
-    emit('filter', filteredPages);
-
-    return {
-      filter: (i: number, v: boolean) => {
-        emit('filter', filterAuthor(i, v));
-        eventBus.updateFooter();
-      },
-
-      toggleAge: () => {
-        emit('filter', reversePages());
-      },
-
-      isChecked,
-      authors,
-      isFilterOpen,
-      areItemsReversed,
-      authorIndexMap,
-      toggleFilter,
-    };
-  }
+const {items, persist, reverseOrder} = defineProps({
+  ageOnly:      { type: Boolean as PropType<boolean>,    default: false,             },
+  reverseOrder: { type: Boolean as PropType<boolean>,    default: false,             },
+  items:        { type: Array as PropType<FilterData[]>, default: [] as FilterData[] },
+  persist:      { type: Boolean as PropType<boolean>,    default: true               },
 });
+const emit      = defineEmits(['filter']);
+const eventBus  = useEventBus();
 
+const {
+  toggleFilter,
+  filterAuthor,
+  reversePages,
+  authors,
+  isFilterOpen,
+  areItemsReversed,
+  filteredPages,
+  authorIndexMap,
+} = usePageFilter(items, persist, reverseOrder);
 
+emit('filter', filteredPages);
 
-function usePageFilter(pages: FilterData[], isPersisting: boolean, areReversed = false) {
-  const cache            = useDateCache<DataCacheFilterObj>();
-  const filterStore      = cache.getObjData('filter').value;
-  const clonedPages      = pages.slice();
-  const authors          = getAuthors(clonedPages);
-  const isFilterOpen     = ref(filterStore.isPersisting && filterStore.isOpen || false);
-  const areItemsReversed =
-    filterStore.isPersisting
-      ? areReversed ? !filterStore.reversed : filterStore.reversed
-      : areReversed
-  ;
-  const authorIndexMap   =
-    filterStore.isPersisting
-      ? filterStore.authorIndexMap
-      : authors.map((a, i) => i)
-  ;
-  let filteredPages      =
-    filterStore.isPersisting
-      ? filterStore.pages
-      : clonedPages.slice()
-  ;
-
-  if (filterStore.isPersisting && filterStore.reversed) {
-    clonedPages.reverse();
-  }
-
-  cache.updObjData('filter', 'isPersisting', isPersisting);
-  cache.updObjData('filter', 'authorIndexMap', authorIndexMap);
-  cache.updObjData('filter', 'pages', filteredPages);
-
-  return {
-    toggleFilter: () => {
-      isFilterOpen.value = !isFilterOpen.value;
-      cache.updObjData('filter', 'isOpen', isFilterOpen.value);
-    },
-
-    reversePages: () => {
-      // Original pages must also reflect reverse order
-      clonedPages.reverse();
-      const pages = filteredPages.reverse().slice();
-      cache.updObjData('filter', 'reversed', !filterStore.reversed);
-      return pages;
-    },
-
-    filterAuthor: (index: number, val: boolean) => {
-      if (val)  authorIndexMap.push(index);
-      if (!val) authorIndexMap.splice(authorIndexMap.indexOf(index), 1)
-      ;
-      cache.updObjData('filter', 'authorIndexMap', authorIndexMap);
-      filteredPages = clonedPages.filter(item => {
-        return authorIndexMap.some(i => authors[i] == item.author);
-      });
-      cache.updObjData('filter', 'pages', filteredPages);
-      return filteredPages;
-    },
-
-    authors,
-    isFilterOpen,
-    filteredPages,
-    authorIndexMap,
-    areItemsReversed,
-  };
+function filter(i: number, v: boolean) {
+  emit('filter', filterAuthor(i, v));
+  eventBus.updateFooter();
 }
 
-
-
-function getAuthors(pages: FilterData[]) {
-  return pages.reduce((authors, cpg) => {
-    if (authors.includes(cpg.author)) return authors;
-    authors.push(cpg.author);         return authors;
-  }, [] as string[]);
-}
-
+function toggleAge() { emit('filter', reversePages()); }
 
 </script>
 
---
+++ src/composeables/pageFilter.ts
@@ -0,0 +1,85 @@
+
+import { DataCacheFilterObj, useDateCache } from "@/state/cache-state";
+import { ref } from "vue";
+
+
+export interface FilterData {
+  // Allow ignorable props
+  [key: string]: any;
+  author: string;
+  date: string;
+}
+
+
+export function usePageFilter(pages: FilterData[], isPersisting: boolean, areReversed = false) {
+  const cache            = useDateCache<DataCacheFilterObj>();
+  const filterStore      = cache.getObjData('filter').value;
+  const clonedPages      = pages.slice();
+  const authors          = getAuthors(clonedPages);
+  const isFilterOpen     = ref(filterStore.isPersisting && filterStore.isOpen || false);
+  const areItemsReversed =
+    filterStore.isPersisting
+      ? areReversed ? !filterStore.reversed : filterStore.reversed
+      : areReversed
+  ;
+  const authorIndexMap   =
+    filterStore.isPersisting
+      ? filterStore.authorIndexMap
+      : authors.map((a, i) => i)
+  ;
+  let filteredPages      =
+    filterStore.isPersisting
+      ? filterStore.pages
+      : clonedPages.slice()
+  ;
+
+  if (filterStore.isPersisting && filterStore.reversed) {
+    clonedPages.reverse();
+  }
+
+  cache.updObjData('filter', 'isPersisting', isPersisting);
+  cache.updObjData('filter', 'authorIndexMap', authorIndexMap);
+  cache.updObjData('filter', 'pages', filteredPages);
+
+  return {
+    toggleFilter: () => {
+      isFilterOpen.value = !isFilterOpen.value;
+      cache.updObjData('filter', 'isOpen', isFilterOpen.value);
+    },
+
+    reversePages: () => {
+      // Original pages must also reflect reverse order
+      clonedPages.reverse();
+      const pages = filteredPages.reverse().slice();
+      cache.updObjData('filter', 'reversed', !filterStore.reversed);
+      return pages;
+    },
+
+    filterAuthor: (index: number, val: boolean) => {
+      if (val)  authorIndexMap.push(index);
+      if (!val) authorIndexMap.splice(authorIndexMap.indexOf(index), 1)
+      ;
+      cache.updObjData('filter', 'authorIndexMap', authorIndexMap);
+      filteredPages = clonedPages.filter(item => {
+        return authorIndexMap.some(i => authors[i] == item.author);
+      });
+      cache.updObjData('filter', 'pages', filteredPages);
+      return filteredPages;
+    },
+
+    authors,
+    isFilterOpen,
+    filteredPages,
+    authorIndexMap,
+    areItemsReversed,
+  };
+}
+
+
+
+function getAuthors(pages: FilterData[]) {
+  return pages.reduce((authors, cpg) => {
+    if (authors.includes(cpg.author)) return authors;
+    authors.push(cpg.author);         return authors;
+  }, [] as string[]);
+}
