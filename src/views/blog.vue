b476be7b8bb1c584d125b6005422a5a264d767a0
Jason <exanix@gmail.com>
2021-08-05 16:40:45 -0700

    upd(Blog): convert to `script setup`
    ref #65

--- src/views/blog.vue
+++ src/views/blog.vue
@@ -1,26 +1,14 @@
-<template>
-  <app-literature :options="options" />
-</template>
+<template><app-literature :options="options" /></template>
 
+<script lang='ts' setup>
+import AppLiterature, { AppLitOptions } from "@/components/AppLiterature/AppLiterature.vue";
 
+const options: AppLitOptions = {
+  uri          : 'blog',
+  title        : 'Blog Entries',
+  expanded     : true,
+  reverseOrder : true,
+};
 
-<script lang='ts'>
-import { defineComponent } from "vue"
-;
-import AppLiteratureVue, { AppLitOptions } from "@/components/AppLiterature/AppLiterature.vue";
-
-
-export default defineComponent({
-  components: { 'app-literature': AppLiteratureVue, },
-  setup() {
-    const options: AppLitOptions = {
-      uri          : 'blog',
-      title        : 'Blog Entries',
-      expanded     : true,
-      reverseOrder : true,
-    };
-    return { options };
-  }
-});
 </script>
 
