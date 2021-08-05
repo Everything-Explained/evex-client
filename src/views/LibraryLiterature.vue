acbadb159dc693480e22f95a0fbb2faa1a481b17
Jason <exanix@gmail.com>
2021-08-05 16:31:09 -0700

    upd(LibraryLiterature): convert to `script setup`
    ref #65

--- src/views/LibraryLiterature.vue
+++ src/views/LibraryLiterature.vue
@@ -1,23 +1,10 @@
-<template>
-  <app-literature :options="options" />
-</template>
+<template><app-literature :options="options" /></template>
 
+<script lang="ts" setup>
+import AppLiterature, { AppLitOptions } from "@/components/AppLiterature/AppLiterature.vue";
 
-<script lang="ts">
-import { defineComponent } from "vue"
-;
-import AppLiteratureVue, { AppLitOptions } from "@/components/AppLiterature/AppLiterature.vue";
-
-
-export default defineComponent({
-  components: { 'app-literature': AppLiteratureVue, },
-  setup() {
-    const options: AppLitOptions = {
-      uri: 'library/literature',
-      title: 'Library Literature'
-    };
-
-    return { options };
-  }
-});
+const options: AppLitOptions = {
+  uri   : 'library/literature',
+  title : 'Library Literature'
+};
 </script>
