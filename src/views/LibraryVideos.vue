631048c646cc81f00bffb7e9deb04a4cb6207b88
Jason <exanix@gmail.com>
2021-08-06 15:29:48 -0700

    upd(LibraryVideos): convert to `script setup`
    ref #65

--- src/views/LibraryVideos.vue
+++ src/views/LibraryVideos.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="lib-vid">
-    <pg-titlebar
+    <page-titlebar
       :ease-in="350"
       :ease-out="350"
       :text="title"
@@ -49,7 +49,7 @@
             </div>
           </div>
         </div>
-        <pg-footer />
+        <page-footer />
       </div>
       <div v-else-if="activePage">
         <ux-filter
@@ -70,73 +70,50 @@
             {{ v.title }}
           </ux-video>
         </div>
-        <pg-footer />
+        <page-footer />
       </div>
     </transition>
   </div>
 </template>
 
 
-<script lang="ts">
-import { computed, defineComponent, ref, watch } from "vue";
+<script lang="ts" setup>
+import { computed, ref, watch } from "vue";
 import useVideos           from "@/composeables/videos";
 import { useDynamicPager } from "@/composeables/dynamicPager";
 import { useDate }         from "@/composeables/date";
 import { isEthan }         from "@/composeables/globals";
 import { Video }           from "@/typings/global-types";
-import PageTitleBarVue     from "@/components/PageTitlebar.vue";
-import PageFooterVue       from "@/components/PageFooter.vue";
-import uxFilterVue         from "@/components/UxFilter.vue";
-import uxVideoVue          from "@/components/UxVideo.vue";
-import UxPreloaderVue from '@/components/UxPreloader.vue';
+import PageTitlebar     from "@/components/PageTitlebar.vue";
+import PageFooter       from "@/components/PageFooter.vue";
+import UxFilter         from "@/components/UxFilter.vue";
+import UxVideo          from "@/components/UxVideo.vue";
+import UxPreloader      from '@/components/UxPreloader.vue';
+
 
 type VideoCategory = { name: string; description: string; videos: Video[] };
 
-export default defineComponent({
-  components: {
-    'ux-preloader': UxPreloaderVue,
-    'pg-titlebar' : PageTitleBarVue,
-    'pg-footer'   : PageFooterVue,
-    'ux-video'    : uxVideoVue,
-    'ux-filter'   : uxFilterVue,
-  },
-  setup() {
-    const videoURI = '/data/library/videos.json';
 
-    const { setDynPages, goTo, activePage, } = useDynamicPager('library/videos');
-
-    const {
-      videos: categories,
-      isLoadingVideos
-    } = useVideos<VideoCategory>(videoURI, (vidCats) => {
-      setDynPages(vidCats.map(cat => ({ name: cat.name, data: cat.videos })));
-    });
-
-    // Prevent loading of inherently cached videos
-    watch(() => activePage.value, (page) => {
-      if (!page?.data) { visibleVideos.value = []; }
-    });
-
-    const visibleVideos = ref<Video[]>([]);
-    function onFilter(videos: Video[]) { visibleVideos.value = videos; }
-
-    return {
-      getAuthors:     (videos: Video[]) => videos.reduce(toAuthors, [] as string[]),
-      getLatestVideo: (videos: Video[]) => videos[videos.length - 1],
-      toYouTubeLink:  (id: string)      => `//www.youtube-nocookie.com/embed/${id}?rel=0`,
-      useDate,
-      goTo,
-      isEthan,
-      onFilter,
-      title        : computed(() => activePage.value?.title || 'Video Categories'),
-      categories,
-      isLoadingVideos,
-      activePage,
-      visibleVideos,
-    };
+const { setDynPages, goTo, activePage, }     = useDynamicPager('library/videos');
+const { videos: categories, isLoadingVideos} = useVideos<VideoCategory>(
+  '/data/library/videos.json',
+  (vidCats) => {
+    setDynPages(vidCats.map(cat => ({ name: cat.name, data: cat.videos })));
   }
+);
+
+// Prevent loading of inherently cached videos
+watch(() => activePage.value, (page) => {
+  if (!page?.data) { visibleVideos.value = []; }
 });
 
+const visibleVideos = ref<Video[]>([]);
+const title         = computed(() => activePage.value?.title || 'Video Categories');
+
+const onFilter       = (videos: Video[]) => visibleVideos.value = videos;
+const getAuthors     = (videos: Video[]) => videos.reduce(toAuthors, [] as string[]);
+const getLatestVideo = (videos: Video[]) => videos[videos.length - 1];
+const toYouTubeLink  = (id: string)      => `//www.youtube-nocookie.com/embed/${id}?rel=0`;
 
 
 function toAuthors(authors: string[], video: Video) {
@@ -144,7 +121,6 @@ function toAuthors(authors: string[], video: Video) {
   authors.push(video.author);
   return authors;
 }
-
 
 </script>
 
