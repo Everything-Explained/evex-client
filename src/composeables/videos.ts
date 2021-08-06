b1ec85b24425efd5877d2da2ce4c1306a955377d
Jason <exanix@gmail.com>
2021-08-06 15:05:42 -0700

    upd(useVideos): convert `computed ref` to plain `ref`
    Having a computed ref was more trouble than it was worth
    and ultimately unnecessary.

--- src/composeables/videos.ts
+++ src/composeables/videos.ts
@@ -1,11 +1,12 @@
 import { APIResponse, useAPI } from "@/services/api_internal";
 import { DataCacheArrayKeys, useDateCache } from "@/state/cache-state";
+import { Ref, ref } from "@vue/reactivity";
 
 
 export default function useVideos<T>(uri: DataCacheArrayKeys, onVideosReady: (videos: T[]) => void) {
   const dataCache = useDateCache<T>();
   const api       = useAPI();
-  const videos    = dataCache.getArrayData(uri);
+  const videos    = ref(dataCache.getArrayData(uri).value) as Ref<T[]>;
 
   videos.value.length
     ? onVideosReady(videos.value)
@@ -14,6 +15,7 @@ export default function useVideos<T>(uri: DataCacheArrayKeys, onVideosReady: (vi
 
   function cacheVideos(res: APIResponse<T[]>) {
     dataCache.setArrayData(uri, res.data);
+    videos.value = res.data;
     onVideosReady(videos.value);
   }
 
