ceaa5aee271eec1c77681218c5bbdd153ab0f31f
Jason <exanix@gmail.com>
2021-08-20 15:13:02 -0700

    feat(APIInternal): use version manager and reject with `APIResponse`
    refs #61, #67

--- src/services/api_internal.ts
+++ src/services/api_internal.ts
@@ -1,6 +1,7 @@
 import { reactive, computed } from "vue";
 import { isProduction } from "../globals";
 import wretch from 'wretch';
+import { useVersionManager } from "./versionManager";
 
 
 export interface APIResponse<T> {
@@ -36,7 +37,7 @@ const state = reactive({
   userid        : localStorage.getItem('userid') || genUniqueID(),
   isInitialized : false,
   initializing  : false,
-  version       : localStorage.getItem('version') || '',
+  version       : localStorage.getItem('version') || undefined,
   isLoading     : false,
   isDebouncing  : false,
 });
@@ -58,12 +59,31 @@ async function init() {
   // Is always a valid userid
   localStorage.setItem('userid', state.userid)
   ;
-  const res = await API.get<{version: string}>('/auth/setup', { userid: state.userid });
-  // User id created
-  if (res.status == 201) localStorage.setItem('passcode', 'no');
-  if (!state.version || state.version != res.data.version) {
-    localStorage.setItem('version', res.data.version);
-    state.version = res.data.version;
+  const res =
+    await API
+      .get<{version: string}>('/auth/setup', { userid: state.userid })
+      .catch((err: APIResponse<string>) => {
+        return err;
+      })
+  ;
+  const verMan = useVersionManager(state.version);
+  if (typeof res.data != 'string') {
+    // User id created
+    if (res.status == 201) localStorage.setItem('passcode', 'no');
+    if (!state.version || state.version != res.data.version) {
+      localStorage.setItem('version', res.data.version);
+      state.version = res.data.version;
+      verMan.addVersion(state.version);
+    }
+  }
+
+  if (res.status == 521) {
+    console.log(verMan);
+    console.warn('Skipping User Authentication');
+    if (!verMan.version)
+      console.warn('No Versions available to fallback on')
+    ;
+    else console.warn(`Falling back on version: ${verMan.version}`);
   }
   state.isInitialized = true;
 }
@@ -75,15 +95,19 @@ function callAPI<T>(opts: APIOptions): Promise<APIResponse<T>> {
     if (debounceOnPending(rs, () => callAPI(opts))) return;
     state.isLoading = true;
     const api = setupAPI(opts);
+    const serverIsDown = () => rj({ status: 521, data: 'Server is Down' } as APIResponse<string>);
     api
       // Catches Netork Errors
-      .fetchError(rj)
+      .fetchError(serverIsDown)
       // Cloudlfare tells us server is down
-      .error(521, rj)
+      .error(521, serverIsDown)
       .res(async (res) => rs({
         status: res.status,
         data: opts.method == 'get' ? await res.json() : await res.text()
       }))
+      .catch(err =>
+        rj({ status: 0, data: err.toString() } as APIResponse<string>)
+      )
       .finally(() => state.isLoading = false)
     ;
   });
@@ -94,7 +118,7 @@ function setupAPI(opts: APIOptions) {
   const { URI, method, body, type } = opts
   ;
   if ((type || 'dynamic') == 'static')
-    body.version = state.version
+    body.version = state.version || ''
   ;
   const api = (method == 'get')
     ? apiEndpoint.url(URI).query(body)[method]()
