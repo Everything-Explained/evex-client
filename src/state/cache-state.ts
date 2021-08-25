3e4cea699db88dfca57054ce404f267de5fc7a81
Jason <exanix@gmail.com>
2021-08-25 13:49:43 -0700

    clean(cache-state): refactor into explicit functions

--- src/state/cache-state.ts
+++ src/state/cache-state.ts
@@ -42,52 +42,62 @@ export type DataCacheKeys      = keyof typeof stateOfPrimitives;
 // NOTE: We are assuming that all contained non-primitive types will
 //       NOT be mutated. E.g. [Object, Object] or { foo: ['bar'] }
 export function useDateCache<T>() {
+
+  function setArrayData(key: DataCacheArrayKeys, val: any[]) {
+    catchMissingState(key as string, stateOfArrays);
+    (stateOfArrays[key] as any[]) = val.slice();
+  }
+
+  function updArrayData(key: DataCacheArrayKeys, val: any) {
+    catchMissingState(key as string, stateOfArrays);
+    (stateOfArrays[key] as any[]).push(val);
+  }
+
+  function getArrayData (key: DataCacheArrayKeys) {
+    catchMissingState(key as string, stateOfArrays);
+    return computed(() => (stateOfArrays[key]?.slice() || []) as T[]);
+  }
+
+  function setObjData (key: DataCacheObjKeys, val: any) {
+    catchMissingState(key as string, stateOfObjects);
+    stateOfObjects[key] = { ...val };
+  }
+
+  function updObjData (key: DataCacheObjKeys, prop: keyof T, val: any) {
+    catchMissingState(key as string, stateOfObjects);
+    const data = (stateOfObjects[key] as any)[prop];
+    const hasSameType =
+      (typeof data == typeof val) && (data instanceof Array == val instanceof Array)
+    ;
+    if (undefined == data) throw Error(`Property "${prop}" does not exist on Object`);
+    if (!hasSameType)      throw Error('Value must be of the same type as the Object Property');
+    (stateOfObjects[key] as any)[prop] = val;
+  }
+
+  function getObjData (key: DataCacheObjKeys) {
+    catchMissingState(key as string, stateOfObjects);
+    return computed(() => ({ ...(stateOfObjects[key] as any) } as T));
+  }
+
+  function setData (key: DataCacheKeys, val: string|boolean|number) {
+    catchMissingState(key as string, stateOfPrimitives);
+    (stateOfPrimitives[key] as any) = val;
+  }
+
+  function getData (key: DataCacheKeys) {
+    catchMissingState(key as string, stateOfPrimitives);
+    return computed(() => (stateOfPrimitives[key] as any) as T);
+  }
+
+  function catchMissingState(key: string, state: any) {
+    if (undefined == state[key]) throw Error(`Missing Cache Entry::${key}`);
+  }
+
   return {
-    setArrayData: (key: DataCacheArrayKeys, val: any[]) => {
-      catchMissingState(key as string, stateOfArrays);
-      (stateOfArrays[key] as any[]) = val.slice();
-    },
-    updArrayData: (key: DataCacheArrayKeys, val: any) => {
-      catchMissingState(key as string, stateOfArrays);
-      (stateOfArrays[key] as any[]).push(val);
-    },
-    getArrayData: (key: DataCacheArrayKeys) => {
-      catchMissingState(key as string, stateOfArrays);
-      return computed(() => (stateOfArrays[key]?.slice() || []) as T[]);
-    },
-
-    setObjData: (key: DataCacheObjKeys, val: any) => {
-      catchMissingState(key as string, stateOfObjects);
-      stateOfObjects[key] = { ...val };
-    },
-    updObjData: (key: DataCacheObjKeys, prop: keyof T, val: any) => {
-      catchMissingState(key as string, stateOfObjects);
-      const data = (stateOfObjects[key] as any)[prop];
-      const hasSameType =
-        (typeof data == typeof val) && (data instanceof Array == val instanceof Array)
-      ;
-      if (undefined == data) throw Error(`Property "${prop}" does not exist on Object`);
-      if (!hasSameType)      throw Error('Value must be of the same type as the Object Property');
-      (stateOfObjects[key] as any)[prop] = val;
-    },
-    getObjData: (key: DataCacheObjKeys) => {
-      catchMissingState(key as string, stateOfObjects);
-      return computed(() => ({ ...(stateOfObjects[key] as any) } as T));
-    },
-
-    setData: (key: DataCacheKeys, val: string|boolean|number) => {
-      catchMissingState(key as string, stateOfPrimitives);
-      (stateOfPrimitives[key] as any) = val;
-    },
-    getData: (key: DataCacheKeys) => {
-      catchMissingState(key as string, stateOfPrimitives);
-      return computed(() => (stateOfPrimitives[key] as any) as T);
-    }
-  };
-}
-
-function catchMissingState(key: string, state: any) {
-  if (undefined == state[key]) throw Error(`Missing Cache Entry::${key}`);
+    setArrayData, updArrayData, getArrayData,
+    setObjData,   updObjData,   getObjData,
+    setData,                    getData,
+   };
 }
 
 
