399162c15d969ad0c17bc55a29735c3d0740c914
Jason <exanix@gmail.com>
2021-08-24 18:45:30 -0700

    clean(event-bus): refactor with explicit functions

--- src/state/event-bus.ts
+++ src/state/event-bus.ts
@@ -2,9 +2,9 @@
 
 type ValidEvents = 'update-footer'|'update-menu';
 type MenuEventRoutes = 'home'      | 'blog'       | 'support'   |
-                      'lib-lit'   | 'lib-videos' | 'red-login' |
-                      'red-lit'   | 'red-videos' | 'red-form'  |
-                      'changelog' | '404'
+                       'lib-lit'   | 'lib-videos' | 'red-login' |
+                       'red-lit'   | 'red-videos' | 'red-form'  |
+                       'changelog' | '404'
 ;
 type MenuEventCallback = (route: MenuEventRoutes, visibility: boolean) => void
 type CallBack = (...args: any[]) => void;
@@ -28,13 +28,27 @@ export function useEventBus() {
     if (!addedSub) throw Error(`Missing Event::"${ev}"`);
   }
 
-  return {
-    off: (cb: CallBack) => {
-      _subscribers.forEach((sub, i) => sub[1] == cb && _subscribers.splice(i, 1));
-    },
-    onUpdateMenu:   (cb: MenuEventCallback)                      => on('update-menu', cb),
-    updateMenu:     (route: MenuEventRoutes, visibility: boolean) => emit('update-menu', route, visibility),
-    onUpdateFooter: (cb: CallBack)                               => on('update-footer', cb),
-    updateFooter:   ()                                           => emit('update-footer'),
-  };
+  function off(cb: CallBack) {
+    _subscribers.forEach(
+      (sub, i) => sub[1] == cb && _subscribers.splice(i, 1)
+    );
+  }
+
+  function onUpdateMenu(cb: MenuEventCallback) {
+    on('update-menu', cb);
+  }
+
+  function updateMenu(route: MenuEventRoutes, visibility: boolean) {
+    emit('update-menu', route, visibility);
+  }
+
+  function onUpdateFooter(cb: CallBack) {
+    on('update-footer', cb);
+  }
+
+  function updateFooter() {
+    emit('update-footer');
+  }
+
+  return { off, onUpdateMenu, updateMenu, onUpdateFooter, updateFooter, };
 }
