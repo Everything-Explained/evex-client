8e89255bb26875c119eb1fd593bdee29161a559c
jaeiya <exanix@gmail.com>
2022-05-30 11:51:05 -0700

    clean(AppMenu): refactor element declarations to reduce type-hacks
    This also adds a function to verify that all elements exist. This is
    still a "clean" action because if an element did not exist before,
    it would still throw. The difference now is that we throw a custom
    message.

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,32 +1,38 @@
 
 
 <script lang='ts' setup>
-import { computed, onMounted, ref, watch, Ref } from "vue";
-import { useDataCache } from "@/state/cache-state";
-import { useRouteMap }  from "@/composeables/routeMap";
-import { useEventBus }  from "@/state/event-bus";
-import UxIcon           from '@/components/UxIcon.vue';
+import { onMounted, ref, watch } from "vue";
+import { useDataCache }          from "@/state/cache-state";
+import { useRouteMap }           from "@/composeables/routeMap";
+import { useEventBus }           from "@/state/event-bus";
+import UxIcon                    from '@/components/UxIcon.vue';
+
 
 
 interface ExternalElements {
-  body        ?: HTMLElement;
-  header      ?: HTMLElement;
-  menu        ?: Ref<HTMLDivElement>;
-  closeHelper ?: HTMLDivElement;
+  main        : HTMLElement;
+  body        : HTMLElement;
+  header      : HTMLElement;
+  menu        : HTMLDivElement;
+  closeHelper : HTMLDivElement;
 }
+
 
 
 const props         = defineProps({
   contentId : { type: String, required: true },
   headerId  : { type: String, required: true },
 });
-const menuElRef     = ref<HTMLDivElement|null>(null);
+
+const menu          = ref<HTMLDivElement>();
 const opened        = ref(false);
 const dataCache     = useDataCache<boolean>();
 const isMenuOpening = dataCache.getData('titlebar-menu-open');
 const routeMap      = useRouteMap();
-const els           = { menu: computed(() => menuElRef.value!) } as ExternalElements;
-const menu          = menuElRef; // From DOM
+
+let els: ExternalElements;
+
+
 
 useEventBus().on('update-menu', (routeName, visibility) => {
   for (const routeCat of routeMap) {
@@ -35,43 +41,53 @@ useEventBus().on('update-menu', (routeName, visibility) => {
   }
 });
 
-onMounted(setup);
-watch(() => isMenuOpening.value, toggleMenu);
-
-function setup() {
-  els.body        = document.getElementById(props.contentId)!;
-  els.header      = document.getElementById(props.headerId)!;
-  els.closeHelper = document.createElement('div');
+onMounted(() => {
+  els = {
+    // FIX: We should NOT ASSUME that <main> exists
+    main: document.getElementsByTagName('main')![0],
+    body: document.getElementById(props.contentId)!,
+    header: document.getElementById(props.headerId)!,
+    menu: menu.value!,
+    closeHelper: document.createElement('div'),
+  };
+  verifyEls(els);
   els.closeHelper.classList.add('app-menu__close-menu-area');
   els.closeHelper.addEventListener('mousedown', closeMenu);
-  // FIX: We should NOT ASSUME that <main> exists
-  const mainEl = document.getElementsByTagName('main')![0];
-  mainEl.appendChild(els.closeHelper);
+  els.main.appendChild(els.closeHelper);
   affixOnScroll();
-}
+});
 
-function toggleMenu(state: boolean) {
-  opened.value = state;
-  els.body?.classList[state ? 'add' : 'remove']('--menu-open');
-  els.closeHelper?.classList[state ? 'add' : 'remove']('--menu-open');
+watch(() => isMenuOpening.value, toggleMenu);
+
+
+function verifyEls(els: ExternalElements) {
+  let key: keyof typeof els;
+  for (key in els) {
+    if (!els[key]) throw Error(`<app-menu>::missing element::${key}`);
+  }
 }
 
 function affixOnScroll() {
   document.body.addEventListener('scroll', () => {
     const scrollTop = document.body.scrollTop;
-    const menu      = els.menu!;
-    const pos       = menu.value.style.position
+    const menu      = els.menu;
+    const pos       = menu.style.position
     ;
-    if (scrollTop >= els.header!.offsetHeight + 1) {
-      if (pos != 'fixed') menu.value.style.position = 'fixed';
+    if (scrollTop >= els.header.offsetHeight + 1) {
+      if (pos != 'fixed') menu.style.position = 'fixed';
       return;
     }
-    menu.value.style.position = 'absolute';
+    menu.style.position = 'absolute';
   });
 }
 
-function closeMenu() { dataCache.setData('titlebar-menu-open', false); }
+function toggleMenu(state: boolean) {
+  opened.value = state;
+  els.body.classList[state ? 'add' : 'remove']('--menu-open');
+  els.closeHelper.classList[state ? 'add' : 'remove']('--menu-open');
+}
 
+function closeMenu() { dataCache.setData('titlebar-menu-open', false); }
 
 </script>
 
