ac86ba2ced1439dc16dcd61936e0ea2ffa6c3215
jaeiya <exanix@gmail.com>
2022-06-02 08:20:24 -0700

    feat(preloader): add image placeholder animation
    closes #78

--- src/components/UxImg.vue
+++ src/components/UxImg.vue
@@ -2,6 +2,7 @@
   <div ref="containerRef" class="ux-img">
     <ux-preloader
       v-if="state.showPreloader"
+      type="image"
       :page="false"
       :class="['ux-img__preloader', { '--disabled': state.loaded }]"
     />
--- src/components/UxPreloader.vue
+++ src/components/UxPreloader.vue
@@ -1,10 +1,13 @@
 
 
 <script lang='ts' setup>
+import { PropType } from 'vue';
+
 
 defineProps({
   // If preloader spans whole page
-  page: { type: Boolean, default: true }
+  page: { type: Boolean, default: true },
+  type: { type: String as PropType<'ripple'|'image'>,  default: 'ripple'    }
 });
 
 </script>
@@ -14,7 +17,13 @@ defineProps({
 
 
 <template>
-  <div :class="['preloader', { 'page': page }]" />
+  <div v-if="type == 'ripple'" :class="['preloader', { 'page': page }]" />
+  <div
+    v-else-if="type == 'image'"
+    :class="['.preloader', 'preloader__image-wrapper']"
+  >
+    <div class="preloader__image-placeholder" />
+  </div>
 </template>
 
 
