23944286edb2ebebe174dde1e1a26202c08564ea
jaeiya <exanix@gmail.com>
2022-06-01 21:39:45 -0700

    fix(home): remove Disqus component

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,7 +4,6 @@
     <transition name="fade" mode="out-in">
       <div v-if="pageContent" class="content">
         <render-html :html="pageContent" />
-        <ux-disqus />
         <page-footer />
       </div>
       <ux-preloader v-else />
@@ -24,7 +23,6 @@ import RenderHtml   from '@/components/RenderHtml.vue';
 import { useAPI } from '@/services/api_internal';
 import { ref } from 'vue';
 import UxPreloader from '@/components/UxPreloader.vue';
-import UxDisqus    from '@/components/UxDisqus.vue';
 
 const api = useAPI();
 const pageContent = ref('');
