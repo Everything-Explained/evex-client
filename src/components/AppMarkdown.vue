780d7b37d4ead07173746200d085bde986cac5b8
jaeiya <exanix@gmail.com>
2022-09-13 16:58:08 -0700

    feat(app-markdown): add custom email rendering

--- src/components/AppMarkdown.vue
+++ src/components/AppMarkdown.vue
@@ -1,9 +1,23 @@
 
-<script lang="ts" setup>
-defineProps({
+<script lang="ts" setup>import { emailRecipients, getEmail } from '@/globals';
+import { ref } from 'vue';
+
+const {html} = defineProps({
   simple : { type: Boolean, default: true  },
   header : { type: Boolean, default: false },
+  html   : { type: String,  default: '',  required: false },
 });
+
+const customHTMLContent = ref(html);
+renderEmail();
+
+function renderEmail() {
+  for (const name of emailRecipients) {
+    const emailStr = getEmail(name);
+    const emailLink = `<a href="mailto:${emailStr}?subject=EvEx - Website Feedback">${emailStr}</a>`;
+    customHTMLContent.value = customHTMLContent.value.replaceAll(`@${name}@`, emailLink);
+  }
+}
 </script>
 
 
@@ -17,7 +31,8 @@ defineProps({
       { 'md-simple': simple && !header },
     ]"
   >
-    <slot />
+    <slot v-if="!html" />
+    <div v-else v-html="customHTMLContent" />
   </div>
 </template>
 
