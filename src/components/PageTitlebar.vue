ef463fcbac8323a505eb79c3ba779934e9e5d865
jaeiya <exanix@gmail.com>
2022-09-24 15:06:26 -0700

    feat(titlebar): add normal and red33m gradient styles
    ref #88

--- src/components/PageTitlebar.vue
+++ src/components/PageTitlebar.vue
@@ -1,53 +1,45 @@
-
-
-<script lang='ts' setup>
-import { useDataCache } from "@/state/cache-state";
+<script lang="ts" setup>
+import { useDataCache } from '@/state/cache-state';
 import UxIcon from '@/components/UxIcon.vue';
 
-
 const props = defineProps({
-  easeIn:  { type: Number, default: 400 },
+  easeIn: { type: Number, default: 400 },
   easeOut: { type: Number, default: 400 },
-  text:    { type: String, default: '' }
+  text: { type: String, default: '' },
+  isRed33m: { type: Boolean, default: false },
+  noFade: { type: Boolean, default: false },
 });
 
-const stateStr   = 'titlebar-menu-open';
-const dataCache  = useDataCache<boolean>();
+const stateStr = 'titlebar-menu-open';
+const dataCache = useDataCache<boolean>();
 const isMenuOpen = dataCache.getData(stateStr);
-const duration   = {
+const duration = {
   enter: props.easeIn,
-  leave: props.easeOut
+  leave: props.easeOut,
 };
 
 const openMenu = () => dataCache.setData(stateStr, true);
-
 </script>
 
-
-
-
-
 <template>
-  <div class="pg-titlebar">
-    <ux-icon
-      :class="['pg-titlebar__menu-icon', { '--menu-open': isMenuOpen }]"
-      :type="'menu'"
-      @mousedown="openMenu"
-    />
-    <transition
-      name="fade"
-      :duration="duration"
-      mode="out-in"
-    >
-      <div :key="text" class="pg-titlebar__text">
-        {{ text }} <slot />
-      </div>
-    </transition>
+  <div class="titlebar-container">
+    <div :class="['titlebar-ribbon', { red33m: isRed33m }]" />
+    <div :class="['pg-titlebar', { red33m: isRed33m }]">
+      <ux-icon
+        :class="[
+          'pg-titlebar__menu-icon',
+          { '--menu-open': isMenuOpen, red33m: isRed33m },
+        ]"
+        :type="'menu'"
+        @mousedown="openMenu"
+      />
+      <transition name="fade" :duration="duration" mode="out-in">
+        <div :key="text" :class="['pg-titlebar__text', { red33m: isRed33m }]">
+          {{ text }} <slot />
+        </div>
+      </transition>
+    </div>
+    <div :class="['titlebar-ribbon bottom', { red33m: isRed33m }]" />
+    <div v-if="!noFade" class="titlebar-content-fader" />
   </div>
 </template>
-
-
-
-
-
-
