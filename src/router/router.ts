4ce81ed5536badb18a9df7ed88230ada2b20c5ed
jaeiya <exanix@gmail.com>
2022-10-01 15:33:00 -0700

    chore(router): lint file

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,112 +1,133 @@
-import { createRouter,
-         createWebHistory,
-         RouteRecordRaw } from 'vue-router';
-import { isAuthed, isAuthedGuard, isDevelopment }  from '@/globals';
-import Home               from '@/views/HomePage.vue';
-import httpError404       from '@/views/404Error.vue';
-import R3dVideos          from '@/views/Red33mVideos.vue';
-import PublicBlog         from '@/views/PublicBlog.vue';
-import Red33mBlog         from '@/views/Red33mBlog.vue';
-import Videos             from '@/views/LibraryVideos.vue';
-import Literature         from '@/views/LibraryLiterature.vue';
-import Red33mLogin        from '@/views/Red33mLogin.vue';
-import red33mForm         from '@/views/Red33mAccessForm.vue';
-import r3d_litVue         from '@/views/Red33mLiterature.vue';
-import changelogVue       from '@/views/ChangelogPage.vue';
-import AppVideos          from '@/components/AppVideos.vue';
-
-
-
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import { isAuthed, isAuthedGuard, isDevelopment } from '@/globals'
+import Home from '@/views/HomePage.vue'
+import httpError404 from '@/views/404Error.vue'
+import R3dVideos from '@/views/Red33mVideos.vue'
+import PublicBlog from '@/views/PublicBlog.vue'
+import Red33mBlog from '@/views/Red33mBlog.vue'
+import Videos from '@/views/LibraryVideos.vue'
+import Literature from '@/views/LibraryLiterature.vue'
+import Red33mLogin from '@/views/Red33mLogin.vue'
+import red33mForm from '@/views/Red33mAccessForm.vue'
+import r3d_litVue from '@/views/Red33mLiterature.vue'
+import changelogVue from '@/views/ChangelogPage.vue'
 
 export const routes: Array<RouteRecordRaw> = [
-  { path        : '/',
-    name        : 'home',
-    component   : Home,
-    meta        : { cat: 'root', title: 'Home', visible: true }
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: { cat: 'root', title: 'Home', visible: true },
   },
-  { path        : '/home',
-    name        : 'home-redirect',
-    redirect    : '/',
-    meta        : { cat: 'root', title: 'Home', visible: false }
+  {
+    path: '/home',
+    name: 'home-redirect',
+    redirect: '/',
+    meta: { cat: 'root', title: 'Home', visible: false },
   },
-  { path        : '/blog/public/:page?',
-    name        : 'blog',
-    component   : PublicBlog,
-    meta        : { cat: 'root', title: 'Blog', visible: true }
+  // { path        : '/test',
+  //   name        : 'test',
+  //   component   : AppVideos,
+  //   meta        : { cat: 'root', title: 'Test', visible: true }
+  // },
+  {
+    path: '/blog/public/:page?',
+    name: 'blog',
+    component: PublicBlog,
+    meta: { cat: 'root', title: 'Blog', visible: true },
   },
-  { path        : '/videos/public/:category?/:id?',
-    name        : 'lib-videos',
-    component   : Videos,
-    meta        : { cat: 'root', title: 'Videos', visible: true }
+  {
+    path: '/videos/public/:category?/:id?',
+    name: 'lib-videos',
+    component: Videos,
+    meta: { cat: 'root', title: 'Videos', visible: true },
   },
-  { path        : '/literature/public/:page?',
-    name        : 'lib-lit',
-    component   : Literature,
-    meta        : { cat: 'root', title: 'Literature', visible: true }
+  {
+    path: '/literature/public/:page?',
+    name: 'lib-lit',
+    component: Literature,
+    meta: { cat: 'root', title: 'Literature', visible: true },
   },
-  { path        : '/support',
-    name        : 'support',
-    component   : () => import('@/views/SupportPage.vue'),
-    meta        : { cat: 'root', title: 'Support', visible: true }
+  {
+    path: '/support',
+    name: 'support',
+    component: () => import('@/views/SupportPage.vue'),
+    meta: { cat: 'root', title: 'Support', visible: true },
   },
-  { path        : '/changelog/:page?',
-    name        : 'changelog',
-    component   : changelogVue,
-    meta        : { cat: 'root', title: 'ChangeLog', visible: true }
+  {
+    path: '/changelog/:page?',
+    name: 'changelog',
+    component: changelogVue,
+    meta: { cat: 'root', title: 'ChangeLog', visible: true },
   },
-  { path        : '/red33m/login',
-    name        : 'red-login',
-    component   : Red33mLogin,
-    meta        : { cat: 'RED33M', title: 'Login', visible: !isAuthed() || isDevelopment }
+  {
+    path: '/red33m/login',
+    name: 'red-login',
+    component: Red33mLogin,
+    meta: {
+      cat: 'RED33M',
+      title: 'Login',
+      visible: !isAuthed() || isDevelopment,
+    },
   },
-  { path        : '/blog/red33m/:page?',
-    name        : 'r3d-blog',
-    component   : Red33mBlog,
-    beforeEnter : isAuthedGuard,
-    meta        : { cat: 'RED33M', catVisible: isAuthed(), title: 'Blog', visible: isAuthed() }
+  {
+    path: '/blog/red33m/:page?',
+    name: 'r3d-blog',
+    component: Red33mBlog,
+    beforeEnter: isAuthedGuard,
+    meta: {
+      cat: 'RED33M',
+      catVisible: isAuthed(),
+      title: 'Blog',
+      visible: isAuthed(),
+    },
   },
-  { path        : '/videos/red33m/:id?',
-    name        : 'r3d-videos',
-    component   : R3dVideos,
-    beforeEnter : isAuthedGuard,
-    meta        : { cat: 'RED33M', title: 'Videos', visible: isAuthed() }
+  {
+    path: '/videos/red33m/:id?',
+    name: 'r3d-videos',
+    component: R3dVideos,
+    beforeEnter: isAuthedGuard,
+    meta: { cat: 'RED33M', title: 'Videos', visible: isAuthed() },
   },
-  { path        : '/literature/red33m/:page?',
-    name        : 'r3d-lit',
-    component   : r3d_litVue,
-    beforeEnter : isAuthedGuard,
-    meta        : { cat: 'RED33M', title: 'Literature', visible: isAuthed() }
+  {
+    path: '/literature/red33m/:page?',
+    name: 'r3d-lit',
+    component: r3d_litVue,
+    beforeEnter: isAuthedGuard,
+    meta: { cat: 'RED33M', title: 'Literature', visible: isAuthed() },
   },
-  { path        : '/red33m/form',
-    name        : 'r3d-form',
-    component   : red33mForm,
-    meta        : { cat: 'Accessory', catVisible: isDevelopment, title: 'R3D Form', visible: true }
+  {
+    path: '/red33m/form',
+    name: 'r3d-form',
+    component: red33mForm,
+    meta: {
+      cat: 'Accessory',
+      catVisible: isDevelopment,
+      title: 'R3D Form',
+      visible: true,
+    },
   },
-  { path        : '/403',
-    name        : '403',
-    component   : () => import('@/views/403Error.vue'),
-    meta        : { cat: 'Accessory', title: '401', visible: isDevelopment }
+  {
+    path: '/403',
+    name: '403',
+    component: () => import('@/views/403Error.vue'),
+    meta: { cat: 'Accessory', title: '401', visible: isDevelopment },
   },
-  { path        : '/:pathMatch(.*)*',
-    name        : '404',
-    component   : httpError404,
-    meta        : { cat: 'Accessory', title: '404', visible: isDevelopment }
-  }
-];
-
+  {
+    path: '/:pathMatch(.*)*',
+    name: '404',
+    component: httpError404,
+    meta: { cat: 'Accessory', title: '404', visible: isDevelopment },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
-
+})
 
 // Hack for globally manipulating router; dynamically injected
 // HTML links use this to navigate to internal pages.
-window.$router = router;
+window.$router = router
 
-
-export default router;
-
-
-
+export default router
