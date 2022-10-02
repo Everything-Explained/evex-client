9a8c619d2bb8eabee97de031edbf2a46ba8dd43b
jaeiya <exanix@gmail.com>
2022-10-02 13:40:38 -0700

    chore(red33m-login): lint file

--- src/views/Red33mLogin.vue
+++ src/views/Red33mLogin.vue
@@ -1,31 +1,27 @@
-
-
-<script lang='ts' setup>
-import { ref } from "vue";
-import { APIResponse, useAPI } from "@/services/api_internal";
-import useInputValidation from "@/composeables/inputValidation";
-import PageTitlebar from "@/components/PageTitlebar.vue";
-import PageFooter from "@/components/PageFooter.vue";
-import FormError from "@/components/FormError.vue";
-import UxButton from "@/components/UxButton.vue";
-import UxInput from "@/components/UxInput.vue";
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { APIResponse, useAPI } from '@/services/api_internal';
+import useInputValidation from '@/composeables/inputValidation';
+import PageTitlebar from '@/components/PageTitlebar.vue';
+import PageFooter from '@/components/PageFooter.vue';
+import FormError from '@/components/FormError.vue';
+import UxButton from '@/components/UxButton.vue';
+import UxInput from '@/components/UxInput.vue';
 import UxText from '@/components/UxText.vue';
-import { useEventBus } from "@/state/event-bus";
+import { useEventBus } from '@/state/event-bus';
 
-
-const api             = useAPI();
-const {isPending}     = api;
-const {isValidated, validate}
-                      = useInputValidation(1);
-const eventBus        = useEventBus();
-const codeLength      = 6;
-const code            = ref('');
-const errorText       = ref('');
-const errorUpdVal     = ref(0);
-const loginSuccess    = ref(false);
+const api = useAPI();
+const { isPending } = api;
+const { isValidated, validate } = useInputValidation(1);
+const eventBus = useEventBus();
+const codeLength = 6;
+const code = ref('');
+const errorText = ref('');
+const errorUpdVal = ref(0);
+const loginSuccess = ref(false);
 
 function setError(res: APIResponse<{ message: string }>) {
-  errorText.value = res.data.message, errorUpdVal.value = Date.now();
+  (errorText.value = res.data.message), (errorUpdVal.value = Date.now());
 }
 
 function submit(e: MouseEvent) {
@@ -41,16 +37,10 @@ function submit(e: MouseEvent) {
         eventBus.updateMenu('red-lit', true);
         eventBus.updateMenu('red-login', false);
       })
-      .catch(setError)
-    ;
+      .catch(setError);
   });
 }
-
 </script>
-
-
-
-
 
 <template>
   <div class="red-login-container">
@@ -63,14 +53,14 @@ function submit(e: MouseEvent) {
     <transition name="fade" mode="out-in">
       <div v-if="!loginSuccess" class="red-login">
         <ux-text type="block" custom-class="red-login__disclaimer">
-          This page contains sensitive content which <em>requires authentication</em>.
-          If you have a passcode, use the form below to grant yourself access.
-          <br><br>
+          This page contains sensitive content which
+          <em>requires authentication</em>. If you have a passcode, use the form
+          below to grant yourself access. <br /><br />
           <strong>Don't have a Passcode?</strong>
-          <br>
+          <br />
           Fill out the
           <router-link to="/red33m/form">
-            {{ "EC Form" }}
+            {{ 'EC Form' }}
           </router-link>
           to gain eligibility; filling out the form <em>does not</em>
           guarantee a Passcode, it only makes you eligible.
@@ -96,50 +86,45 @@ function submit(e: MouseEvent) {
           >
             ENTER
           </ux-button>
-          <br>
+          <br />
           <form-error
             custom-class="error"
             :update="errorUpdVal"
             :text="errorText"
           />
         </form>
-        <br>
+        <br />
         <ux-text type="span-block" custom-class="red-login__disclaimer">
           <strong>NOTE:</strong> Do not clear your browser cache, otherwise you
           will need to enter the code again, when you come back to this page
           later.
         </ux-text>
         <ux-text type="span-block" custom-class="red-login__disclaimer">
-          <strong>CAVEAT:</strong> The passcode will only be saved for <em>this device</em>.
-          In order to view this content on your other devices:
+          <strong>CAVEAT:</strong> The passcode will only be saved for
+          <em>this device</em>. In order to view this content on your other
+          devices:
           <strong>computer, phone, tablet, etc...</strong>
-          You <em>must enter the passcode again</em> on those devices.<br><br>
+          You <em>must enter the passcode again</em> on those devices.<br /><br />
         </ux-text>
         <page-footer />
       </div>
       <div v-else>
         <ux-text type="block">
-          You have <b>successfully</b> gained access to the <em>RED33M</em> section.
-          <br><br>
-          If you open the menu, you will notice that under the RED33M header, there will
-          now be two new options: <b>Literature</b> and <b>Videos</b>.
-          <br><br>
-          Unlike the public Literature page, there are no categories, since they fall under
-          the <em>RED33M</em> category. If you have any general questions or concerns
-          you can goto our <router-link to="/support">Support Page</router-link>. However if you
-          need specific help with something that our Support page does not cover, feel free to
-          email <b>Ethan</b> for any <em>one on one</em> needs.
+          You have <b>successfully</b> gained access to the
+          <em>RED33M</em> section. <br /><br />
+          If you open the menu, you will notice that under the RED33M header,
+          there will now be two new options: <b>Literature</b> and
+          <b>Videos</b>. <br /><br />
+          Unlike the public Literature page, there are no categories, since they
+          fall under the <em>RED33M</em> category. If you have any general
+          questions or concerns you can goto our
+          <router-link to="/support">Support Page</router-link>. However if you
+          need specific help with something that our Support page does not
+          cover, feel free to email <b>Ethan</b> for any
+          <em>one on one</em> needs.
         </ux-text>
         <page-footer />
       </div>
     </transition>
   </div>
 </template>
-
-
-
-
-
-
-
-
